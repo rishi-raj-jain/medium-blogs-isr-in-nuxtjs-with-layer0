04ca42f4728a68c21c2c4418cb6ad932c2b8a567
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2021-09-04 02:56:12 +0530

    fallback

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,9 @@ const path = require('path')
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  generate: {
+    fallback: 'fall.html'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
