3013a17ad367a6926e5b4d28fc70b971c424a428
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2021-09-04 01:25:20 +0530

    Test ISR

--- routes.js
+++ routes.js
@@ -1,57 +1,24 @@
 const { Router } = require("@layer0/core/router");
 const { nuxtRoutes } = require("@layer0/nuxt");
 
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.layer0.co;
-  style-src 'self' 'unsafe-inline' *.googleapis.com;
-  img-src * blob: data:;
-  media-src 'none';
-  connect-src *;
-  font-src 'self' *.gstatic.com;
-`;
-
 module.exports = new Router()
   .match("/service-worker.js", ({ serviceWorker }) => {
     serviceWorker(".nuxt/dist/client/service-worker.js");
   })
-  .get("/", ({ cache, setResponseHeader }) => {
+  .get("/", ({ cache }) => {
     cache({
       edge: {
         maxAgeSeconds: 60 * 60 * 24 * 365
       }
     });
-    setResponseHeader(
-      "Content-Security-Policy",
-      ContentSecurityPolicy.replace(/\n/g, "")
-    );
-    setResponseHeader("Referrer-Policy", "origin-when-cross-origin");
-    setResponseHeader("X-Frame-Options", "DENY");
-    setResponseHeader("X-Content-Type-Options", "nosniff");
-    setResponseHeader("X-DNS-Prefetch-Control", "on");
-    setResponseHeader(
-      "Permissions-Policy",
-      "camera=(), microphone=(), geolocation=()"
-    );
   })
-  .get("/:slug", ({ cache, setResponseHeader }) => {
+  .get("/:slug", ({ cache }) => {
     cache({
       edge: {
         maxAgeSeconds: 60 * 60 * 24 * 365, // keep the incrementally generated page for a year
-        staleWhileRevalidateSeconds: 1 // revalidate the data on page every hour
-      }
+        staleWhileRevalidateSeconds: 1 // revalidate the data on page every second
+      },
+      browser: false
     });
-    setResponseHeader(
-      "Content-Security-Policy",
-      ContentSecurityPolicy.replace(/\n/g, "")
-    );
-    setResponseHeader("Referrer-Policy", "origin-when-cross-origin");
-    setResponseHeader("X-Frame-Options", "DENY");
-    setResponseHeader("X-Content-Type-Options", "nosniff");
-    setResponseHeader("X-DNS-Prefetch-Control", "on");
-    setResponseHeader(
-      "Permissions-Policy",
-      "camera=(), microphone=(), geolocation=()"
-    );
   })
   .use(nuxtRoutes);
